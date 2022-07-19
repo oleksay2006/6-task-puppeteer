7b7b764a41d3ce7ab735bdc5e1b23eeea1e8e4b0
Caesar <oleksay2006@gmail.com>
2022-07-20 01:16:42 +0300

    Image from 4 pieces

--- src/index.js
+++ src/index.js
@@ -1,36 +1,62 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false, slowMo: 25, devtools: true });
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: { width: 8000, height: 8000 },
+  });
   const page = await browser.newPage();
   await page.goto('https://pptr.dev/', {
-    waitUntil: 'networkidle2',
+    waitUntil: 'domcontentloaded',
   });
 
-  await page.evaluate(() => {
-    const background = document.querySelector('.navbar');
-    background.style.background = '#1F54C0';
+  await page.screenshot({
+    path: './images/part1.png',
+    clip: {
+      x: 0,
+      y: 0,
+      width: 4000,
+      height: 4000,
+    },
   });
-  await page.screenshot({ path: '../images/changedBackground.png' });
 
-  await browser.close();
-})();
-
-(async () => {
-  // PDF creation is only supported in headless mode
-  const browser = await puppeteer.launch({ headless: true, slowMo: 200 });
-  const page = await browser.newPage();
-  await page.goto('https://pptr.dev/');
-  await page.focus('.navbar__search-input');
-  await page.keyboard.type('pdf');
-  await page.evaluate(() => {
-    const input = document.querySelector('.navbar__search-input');
-    const KEY_CODE_ENTER = 13;
-    const evt = new Event('keydown');
-    evt.keyCode = KEY_CODE_ENTER;
-    input.dispatchEvent(evt);
+  await page.screenshot({
+    path: './images/part2.png',
+    clip: {
+      x: 4000,
+      y: 0,
+      width: 4000,
+      height: 4000,
+    },
   });
-  await page.pdf({ path: '../pdf/enterOnInput.pdf', format: 'a4' });
+
+  await page.screenshot({
+    path: './images/part3.png',
+    clip: {
+      x: 0,
+      y: 4000,
+      width: 4000,
+      height: 4000,
+    },
+  });
+
+  await page.screenshot({
+    path: './images/part4.png',
+    clip: {
+      x: 4000,
+      y: 4000,
+      width: 4000,
+      height: 4000,
+    },
+  });
+
+  await page.goto('file:///D:/MyProjects/Onix/Nodejs-internship/6-task-puppeteer/src/index.html', {
+    waitUntil: 'domcontentloaded',
+  });
+  await page.screenshot({
+    path: './images/parts1-4.png',
+    fullPage: true,
+  });
 
   await browser.close();
 })();
